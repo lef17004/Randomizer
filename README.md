4f18f3c434a21800129fcc1c55db9cb098564d9f
Michael <lef17004@byui.edu>
2020-11-13 16:38:16 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#  Objective-C Class Test
+
+## Overview
+This program allows you to type in people's names (or anything else) and it will pick one out at random. I made this to learn how to use NSStrings and  NSArrays. 
+
+## Development Envirnment
+I created this using Xcode version 12.1. To set up this project, first install Xcode if you don't have it already. Then go to Source Control->Clone and then paste in the link to this Github repository. The project should download and be setup automatically. Press the build and run button to test the code. 
+
+![Screenshot showing the output of the program.](program.png) 
+
+
+## Useful Websites
+[Techotopia](https://www.techotopia.com/index.php/Objective-C_Inheritance)
+
+[C for Dummies](https://www.dummies.com/programming/c/how-to-generate-random-numbers-in-c-programming/)
+
+[Journal Dev](https://www.journaldev.com/10182/nsarray-nsmutablearray-objective-c-array)
+
+[Tutorialspoint](https://www.tutorialspoint.com/objective_c/index.htm)
+
+[Apple's Ojective-C Book](https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/Introduction/Introduction.html)
+
+[Python Reference](https://docs.python.org/3/tutorial/index.html)
--- randomizer/main.m
+++ randomizer/main.m
@@ -7,10 +7,56 @@
 
 #import <Foundation/Foundation.h>
 
+NSArray* getItems() {
+    
+    NSMutableArray *items = [NSMutableArray arrayWithCapacity:1];
+    while(true) {
+        char item[256];
+        
+        printf("Name %d: ", [items count] + 1);
+        
+        scanf("%s", item);
+        NSString *itemInString = [NSString stringWithUTF8String:item];
+        
+        if ([itemInString  isEqual: @"stop"]) {
+            return items;
+        }
+        
+        [items addObject:itemInString];
+    }
+}
+
+
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        printf("Welcome to Michael's Amazing Random Name Picker! Type in the names of everyone in the group, and it will pick one out at random! Type stop when you have entered in all the names.\n");
+        NSArray* allTheItems = getItems();
+        
+        //Iterate over a NSArray
+//        for (id item in allTheItems) {
+//            printf("%s", [item UTF8String]);
+//        }
+        
+        while(true)
+        {
+            srand((unsigned)time(NULL));
+            int randomNumber = rand() % [allTheItems count];
+            printf("\nThe chosen name is: %s!!!\n", [allTheItems[randomNumber] UTF8String]);
+            printf("Would you like to pick another name? y/n: ");
+            char name[20];
+            scanf("%s", name);
+           if (name[0] != 'y')
+           {
+               return 0;
+           }
+            
+        }
+        
+        
+        
     }
     return 0;
 }
