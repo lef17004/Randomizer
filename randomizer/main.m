b69a1fa7149cb35ae9fd46b35c5397de5d7c63a5
Michael <lef17004@byui.edu>
2020-11-12 20:01:59 -0700

    Initial Commit

---
+++ randomizer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  randomizer
+//
+//  Created by Michael Le Fevre on 11/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
